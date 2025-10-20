bd9208d431334b83c91db154820614c5ce3bc4ae
Joseph Hellerstein <jlheller@uw.edu>
2025-10-19 22:58:01 -0700

    ConvolutionalAutocoder updates

--- src/convolutional_autoencoder.py
+++ src/convolutional_autoencoder.py
@@ -2,6 +2,7 @@ import src.constants as cn  # type: ignore
 import src.util as util  # type: ignore
 from src.abstract_autoencoder import AbstractAutoencoder  # type: ignore
 
+from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
 from tensorflow import keras # type: ignore
@@ -11,7 +12,7 @@ from sklearn.decomposition import PCA # type:ignore
 from sklearn.preprocessing import StandardScaler # type:ignore
 import pandas as pd # type: ignore
 import os
-from typing import List, Tuple, Any
+from typing import List, Tuple, Union, Any
 
 
 BASE_PATH = os.path.join(cn.MODEL_DIR, "convolutional_autoencoder")
@@ -27,17 +28,15 @@ print(f"Test data shape: {X_TEST.shape}")
 
 class ConvolutionalAutoencoder(AbstractAutoencoder):
 
-    def __init__(self, image_shape: List[int],
-            hidden_dims: List[int] = [128, 64, 32],
-            num_detector: int=2, base_path: str=BASE_PATH,
+    def __init__(self, image_shape: Union[Tuple[int], List[int]],
+            filter_sizes: List[int] = [32, 64, 128, 32],
+            base_path: str=BASE_PATH,
             is_delete_serializations: bool=True):
         """Initializes the convolutional autoencoder.
 
         Args:
             image_shape (List[int]): Shape of the input images (height, width, channels)
-            hidden_dims (List[int]): 3 dimensions of the hidden layers.
-                The last layer is the bottleneck (encoding) layer.
-            num_detector (int, optional): Number of convolutional detectors. Defaults to 2.
+            filter_sizes (List[int]): dimensions of the filters. Last one is the filter for the bottleneck
             base_path (str, optional): Base path for model serialization. Defaults to BASE_PATH.
             is_delete_serializations (bool, optional): Whether to delete existing serializations. Defaults to
         """
@@ -46,11 +45,10 @@ class ConvolutionalAutoencoder(AbstractAutoencoder):
         if len(image_shape) == 2:
             image_shape = [image_shape[0], image_shape[1], 1]
         self.image_shape = image_shape
-        if len(hidden_dims) != 3:
-            raise ValueError("hidden_dims must be a list of 3 integers")
-        self.hidden_dims = hidden_dims
+        if len(filter_sizes) == 0:
+            raise ValueError("filter_sizes must have at least one element")
+        self.filter_sizes = filter_sizes
         self.image_size = np.prod(image_shape)
-        self.num_detector = num_detector
         super().__init__(base_path=base_path,
                 is_delete_serializations=is_delete_serializations)
 
@@ -58,96 +56,62 @@ class ConvolutionalAutoencoder(AbstractAutoencoder):
         """Builds the convolutional autoencoder model.
 
         Returns:
-            keras.Model: The constructed autoencoder model.
+            keras.Model, keras.Model, keras.Model, dict: The autoencoder, encoder, decoder models, and history dictionary.
         """
-        # Check if the model already exists
         history_dct: dict = {}
-        # No existing model. Must build a new one.
         input_img = keras.Input(shape=self.image_shape)
-        """
-        # Encoder
-        encoded = layers.Conv2D(num_detector, (3, 3), activation='relu', padding='same')(input_img)
-        encoded = layers.Reshape((num_detector*int(self.image_size/self.image_shape[-1]),))(encoded)
-        encoded = layers.Dense(self.hidden_dims[0], activation='relu')(encoded)
-        encoded = layers.Dense(self.hidden_dims[1], activation='relu')(encoded)
-        encoded = layers.Dense(self.hidden_dims[2], activation='relu')(encoded)
-        # Decoder
-        decoded = layers.Dense(self.hidden_dims[1], activation='relu')(encoded)
-        decoded = layers.Dense(self.hidden_dims[0], activation='relu')(decoded)
-        decoded = layers.Dense(int(self.image_size*int(num_detector/self.image_shape[-1])), activation='relu')(decoded)
-        decoded = layers.Reshape((*self.image_shape[:-1], num_detector))(decoded)
-        decoded = layers.Conv2D(1, (3, 3), activation='relu', padding='same')(decoded)
-        #
-        encoded = layers.Conv2D(self.image_shape[-1], (3, 3), activation='relu', padding='same')(input_img)  # 28×28×num_detector
-        encoded = layers.Reshape((self.image_size,))(encoded)
-        encoded = layers.Dense(self.hidden_dims[0], activation='relu')(encoded)
-        encoded = layers.Dense(self.hidden_dims[1], activation='relu')(encoded)
-        encoded = layers.Dense(self.hidden_dims[2], activation='relu')(encoded)
-        # Decoder
-        decoded = layers.Dense(self.hidden_dims[1], activation='relu')(encoded)
-        decoded = layers.Dense(self.hidden_dims[0], activation='relu')(decoded)
-        decoded = layers.Dense(self.image_size, activation='relu')(decoded)
-        decoded = layers.Reshape((self.image_shape))(decoded)
-        decoded = layers.Conv2D(self.image_shape[-1], (3, 3), activation='relu', padding='same')(decoded)  # 28×28×1
-        """
-        #
-        # No existing model. Must build a new one.
-        input_img = keras.Input(shape=self.image_shape)  # e.g., (28, 28, 1)
-        spatial_size = self.image_shape[0] * self.image_shape[1]
-        # Encoder
-        """
-        encoded = layers.Conv2D(self.num_detector, (3, 3), activation='relu', padding='same')(input_img)
-        encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)
-        encoded = layers.Reshape((self.num_detector*spatial_size//4,))(encoded)
-        encoded = layers.Dense(self.hidden_dims[0], activation='relu')(encoded)
-        encoded = layers.Dense(self.hidden_dims[1], activation='relu')(encoded)
-        encoded = layers.Dense(self.hidden_dims[2], activation='relu')(encoded)
-        # Decoder
-        decoded = layers.Dense(self.hidden_dims[1], activation='relu')(encoded)
-        decoded = layers.Dense(self.hidden_dims[0], activation='relu')(decoded)
-        decoded = layers.Dense(spatial_size*self.num_detector//4, activation='relu')(decoded)
-        decoded = layers.Reshape((self.image_shape[0]//2, self.image_shape[1]//2, self.num_detector))(decoded)
-        decoded = layers.Conv2DTranspose(2*self.num_detector, (3, 3), strides=(2, 2), activation='relu', padding='same')(decoded)
-        decoded = layers.Conv2D(self.image_shape[-1], (3, 3), activation='relu', padding='same')(decoded)
-        """
-        """
-        decoded = layers.Conv2DTranspose(128, (3, 3), strides=(2, 2), activation='sigmoid', padding='same')(encoded)  # 24x24x128
-        decoded = layers.Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='sigmoid', padding='same')(decoded)  # 48x48x64
-        decoded = layers.Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='sigmoid', padding='same')(decoded)  # 96x96x32
-        decoded = layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(decoded)  # 96x96x3
+        
+        if self.image_shape[0] == 96:
+            encoded: Any = None
+            for idx, filter_size in enumerate(self.filter_sizes):
+                if idx == 0:
+                    input = input_img
+                else:
+                    input = encoded
+                encoded = layers.Conv2D(filter_size, (3, 3), activation='relu', padding='same')(input)
+                if idx < len(self.filter_sizes) - 1:
+                    encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)
+            # Downsampling
+            """ encoded = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)  # 96x96x32
+            encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 48x48x32
+            encoded = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(encoded)  # 48x48x64
+            encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 24x24x64
+            encoded = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(encoded)  # 24x24x128
+            encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 12x12x128
 
-        decoded = layers.Conv2D(128, (3, 3), activation='sigmoid', padding='same')(encoded)  # 12x12x128
-        decoded = layers.UpSampling2D((2, 2))(decoded)  # 24x24x128
+            # Bottleneck
+            encoded = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)  # 12x12xnum_detector """
 
-        decoded = layers.Conv2D(64, (3, 3), activation='sigmoid', padding='same')(decoded)  # 24x24x64
-        decoded = layers.UpSampling2D((2, 2))(decoded)  # 48x48x64
+            # Decoder - progressively upsample with transposed convolutions
+            decoded: Any = None
+            decoded_sizes = self.filter_sizes[:-1][::-1]  # reverse order except last
+            for idx, filter_size in enumerate(decoded_sizes):
+                if idx == 0:
+                    input = encoded
+                else:
+                    input = decoded
+                decoded = layers.Conv2DTranspose(filter_size, (3, 3), strides=(2, 2), activation='relu',
+                        padding='same')(input)
+            decoded = layers.Conv2D(self.image_shape[-1], (3, 3), activation='relu', padding='same')(decoded)
 
-        decoded = layers.Conv2D(32, (3, 3), activation='sigmoid', padding='same')(decoded)  # 48x48x32
-        decoded = layers.UpSampling2D((2, 2))(decoded)  # 96x96x32
+            """ ecoded = layers.Conv2DTranspose(128, (3, 3), strides=(2, 2), activation='relu', padding='same')(encoded)  # 24x24x128
+            decoded = layers.Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='relu', padding='same')(decoded)  # 48x48x64
+            decoded = layers.Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='relu', padding='same')(decoded)  # 96x96x32
+            decoded = layers.Conv2D(3, (3, 3), activation='relu', padding='same')(decoded)  # 96x96x3 """
+        else:
+            # Encoder
+            encoded = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)  # 28x28x32
+            encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 14x14x32
+            encoded = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(encoded)  # 14x14x64
+            encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 7x7x64
 
-        decoded = layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(decoded)  
-        encoded = layers.Reshape((self.num_detector*144,))(encoded)  # 12*12=144
-        decoded = layers.Dense(self.hidden_dims[1], activation='sigmoid')(encoded)
-        decoded = layers.Dense(self.hidden_dims[0], activation='sigmoid')(decoded)
-        decoded = layers.Dense(self.image_size, activation='sigmoid')(decoded)
-        decoded = layers.Reshape(self.image_shape)(decoded)  # 12*12=144
-        """
-        # Downsampling
-        encoded = layers.Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)  # 96x96x32
-        encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 48x48x32
-        encoded = layers.Conv2D(64, (3, 3), activation='relu', padding='same')(encoded)  # 48x48x64
-        encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 24x24x64
-        encoded = layers.Conv2D(128, (3, 3), activation='relu', padding='same')(encoded)  # 24x24x128
-        encoded = layers.MaxPooling2D((2, 2), padding='same')(encoded)  # 12x12x128
+            # Bottleneck
+            encoded = layers.Conv2D(2, (3, 3), activation='relu', padding='same')(encoded)  # 7x7x32
 
-        # Bottleneck
-        encoded = layers.Conv2D(self.num_detector, (3, 3), activation='relu', padding='same')(encoded)  # 12x12xnum_detector
-
-        # Decoder - progressively upsample with transposed convolutions
-        decoded = layers.Conv2DTranspose(128, (3, 3), strides=(2, 2), activation='relu', padding='same')(encoded)  # 24x24x128
-        decoded = layers.Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='relu', padding='same')(decoded)  # 48x48x64
-        decoded = layers.Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='relu', padding='same')(decoded)  # 96x96x32
-        decoded = layers.Conv2D(3, (3, 3), activation='sigmoid', padding='same')(decoded)  # 96x96x3
+            # Decoder
+            decoded = layers.Conv2DTranspose(64, (3, 3), strides=(2, 2), activation='relu', padding='same')(encoded)  # 14x14x64
+            decoded = layers.Conv2DTranspose(32, (3, 3), strides=(2, 2), activation='relu', padding='same')(decoded)  # 28x28x32
+            decoded = layers.Conv2D(1, (3, 3), activation='relu', padding='same')(decoded)  # 28x28x1
         # Create and compile the models
         encoder = keras.Model(input_img, encoded, name="encoder")
         encoder.compile(optimizer='adam', loss='mse', metrics=['mae'])
@@ -175,11 +139,40 @@ class ConvolutionalAutoencoder(AbstractAutoencoder):
                 image_arr = np.expand_dims(image_arr, -1)
             if image_arr.shape[1:] != tuple(self.image_shape):
                 raise ValueError(f"Input image shape {image_arr.shape[1:]} != image shape {self.image_shape}")
-        elif image_arr[0].shape[0] != self.hidden_dims[-1]:
-            raise ValueError(f"Input encoded shape {image_arr[0].shape} != encoding shape {self.hidden_dims[-1]}")
         reconstructed = super().predict(image_arr, predictor_type=predictor_type)
         return reconstructed
 
     @property
     def compression_factor(self) -> float:
-        return float(self.image_size / self.hidden_dims[-1])
+        #return float(self.image_size / self.hidden_dims[-1])
+        raise NotImplementedError("Compression factor is not defined for ConvolutionalAutoencoder")
+
+    ExperimentResult = namedtuple('ExperimentResult',
+            ['batch_size', 'num_filter', 'num_epoch', 'history', 'base_path'])
+    @classmethod
+    def runAnimalExperiment(cls, batch_size: int, num_filter: int, num_epoch: int,
+            filter_sizes: list=[512, 256, 128, 32]) -> ExperimentResult:
+        """Run an experiment on the animal dataset.
+
+        Args:
+            batch_size (int): The batch size to use for training.
+            num_filter (int): The number of filters to use in the convolutional layers.
+            num_epoch (int): The number of epochs to train for.
+
+        Returns:
+            ExperimentResult: The result of the experiment.
+        """
+        X_ANIMALS_TRAIN, _, X_ANIMALS_TEST, __, __ = util.getPklAnimals()
+        IMAGE_SHAPE = np.shape(X_ANIMALS_TRAIN[0])
+        cae = cls(image_shape=IMAGE_SHAPE,  # type: ignore
+                filter_sizes=filter_sizes,
+                is_delete_serializations=True)
+        cae.fit(X_ANIMALS_TRAIN, num_epoch=num_epoch, batch_size=batch_size, validation_data=X_ANIMALS_TEST, verbose=1)
+        base_filename = f"animal_experiment-bsize_{batch_size}-filter_{filter_sizes}-epoch_{num_epoch}"
+        base_path = os.path.join(cn.MODEL_DIR, base_filename)
+        cae.serializeAll(base_path=base_path)
+        return cls.ExperimentResult(batch_size=batch_size,
+                num_filter=num_filter,
+                num_epoch=num_epoch,
+                base_path=base_path,
+                history=cae.history)
--- tests/test_convolutional_autoencoder.py
+++ tests/test_convolutional_autoencoder.py
@@ -26,17 +26,15 @@ X_TEST = np.expand_dims(X_TEST, -1)
 class TestConvolutionalAutoencoder(unittest.TestCase):
 
     def setUp(self):
-        if IGNORE_TEST:
-            return
-        self.encode_dims = ENCODE_DIMS
+        self.filter_sizes = ENCODE_DIMS
         self.cae = ConvolutionalAutoencoder(image_shape=IMAGE_SHAPE,
-                hidden_dims=self.encode_dims,
+                filter_sizes=self.filter_sizes,
                 is_delete_serializations=True)
 
     def testConstructor(self):
         if IGNORE_TEST:
             return
-        self.assertEqual(self.cae.hidden_dims, self.encode_dims)
+        self.assertEqual(self.cae.filter_sizes, self.filter_sizes)
         self.assertIsNotNone(self.cae.autoencoder)
         self.assertIsNotNone(self.cae.encoder)
         self.assertIsNotNone(self.cae.decoder)
@@ -45,15 +43,14 @@ class TestConvolutionalAutoencoder(unittest.TestCase):
     def testFitMNIST(self):
         if IGNORE_TEST or not IS_PLOT:
             return
-        self.cae.summary()
-        self.cae.fit(X_TRAIN, num_epoch=10, batch_size=128, validation_data=X_TEST, verbose=1)
+        self.cae.fit(X_TRAIN, num_epoch=20, batch_size=512, validation_data=X_TEST, verbose=1)
         self.cae.summarizeModel()
         self.cae.plot(X_TEST)
         self.assertIsNotNone(self.cae.history)
         self.assertIn('loss', self.cae.history.history)
         # Evaluate serialization
         cae2 = ConvolutionalAutoencoder(image_shape=IMAGE_SHAPE,
-                hidden_dims=self.encode_dims,
+                filter_sizes=self.filter_sizes,
                 is_delete_serializations=False)
         cae2.plot(X_TEST)
         self.assertIsNotNone(cae2.autoencoder)
@@ -64,22 +61,22 @@ class TestConvolutionalAutoencoder(unittest.TestCase):
     def testFitAnimals(self):
         #if IGNORE_TEST or not IS_PLOT:
         #    return
-        encode_dims = [512, 256, 128]
         cae = ConvolutionalAutoencoder(image_shape=[96, 96, 3],
-                hidden_dims=encode_dims,
-                num_detector=32,
-                is_delete_serializations=True)
+                filter_sizes=[32, 16, 8],
+                #filter_sizes=[32, 128, 256, 16],
+                is_delete_serializations=False)
         cae.summary()
-        cae.fit(X_ANIMALS_TRAIN, num_epoch=20, batch_size=128, validation_data=X_ANIMALS_TEST, verbose=1)
+        cae.fit(X_ANIMALS_TRAIN, num_epoch=1000, batch_size=128, validation_data=X_ANIMALS_TEST, verbose=1)
         cae.plot(X_ANIMALS_TEST)
-        self.assertIsNotNone(cae.history)
-        self.assertIn('loss', cae.history.history)
+        self.assertIsNotNone(cae.history_dct)
+        self.assertIn('loss', cae.history_dct)
+        import pdb; pdb.set_trace()
 
     def testEncoderDecoder1(self):
         if IGNORE_TEST:
             return
         cae = ConvolutionalAutoencoder(image_shape=IMAGE_SHAPE,
-                hidden_dims=ENCODE_DIMS,
+                filter_sizes=ENCODE_DIMS,
                 is_delete_serializations=False)
         prediction_arr = cae.predict(X_TEST, predictor_type="encoder")
         x_test = cae.predict(prediction_arr, predictor_type="decoder")
@@ -89,20 +86,19 @@ class TestConvolutionalAutoencoder(unittest.TestCase):
         if IGNORE_TEST:
             return
         cae = ConvolutionalAutoencoder(image_shape=IMAGE_SHAPE,
-                hidden_dims=ENCODE_DIMS,
+                filter_sizes=ENCODE_DIMS,
                 is_delete_serializations=False)
         autoencoder_prediction = cae.predict(X_TEST, predictor_type="autoencoder")
         encoder_prediction = cae.predict(X_TEST, predictor_type="encoder")
         decoder_prediction = cae.predict(encoder_prediction, predictor_type="decoder")
         self.assertTrue(np.all(autoencoder_prediction.shape == X_TEST.shape))
-        self.assertTrue(np.all(encoder_prediction.shape[1] == cae.hidden_dims[-1]))
         self.assertTrue(np.all(decoder_prediction.shape == X_TEST.shape))
 
     def testPlotEncodedLabels(self):
         if IGNORE_TEST:
             return
         cae = ConvolutionalAutoencoder(image_shape=IMAGE_SHAPE,
-                hidden_dims=ENCODE_DIMS,
+                filter_sizes=ENCODE_DIMS,
                 is_delete_serializations=False)
         cae.plotEncoded(X_TEST, LABEL_TEST, max_num_point=300, lim=[-10, 500])
 if __name__ == '__main__':
